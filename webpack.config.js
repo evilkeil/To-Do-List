03fe41949ca0f91aac9357b865d2db0608ea5239
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-07 11:22:26 +0900

    add date fns

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do List',
+    }),
+  ],
+  resolve: {
+    alias: {
+      'date-fns': 'date-fns/esm',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /date-fns/,
+        use: 'expose-loader',
+      },
+    ],
+  },
+};
