a9e25257dbd8d5e56de20eadac2565d7d2a31c32
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-08 21:21:07 +0900

    test taking project data from storage and pushing into array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import './assets/style.css';
 import GetStorageItems from "./modules/storage/getStorage.js";
-import SetStorageItems from "./modules/storage/setStorage.js";
+import setStorageItem from "./modules/storage/setStorage.js";
 import DeleteStorageItem from "./modules/storage/deleteStorageItems.js";
 import checkStorage from "./modules/storage/checkStorage.js";
+import createProject from './modules/otherLogic/projectObj';
 
 
+
+const proj2 = createProject("dance","#3c7794","2023-09-08");
+
+setStorageItem(`project2`, proj2);
+
+
+
+let projectList =[]
+
+  function getProjectsFromStorage(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let value = localStorage.getItem(key);
+      
+        // Check if the key starts with "project"
+        if (key.startsWith("project")) {
+            let p1=GetStorageItems(key);
+            
+          projectList.push(p1);
+        }
+      }
+};
+getProjectsFromStorage();
+console.log(projectList)
 
 
 
