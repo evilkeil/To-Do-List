5f445d6a8fac69464edfac57dfc8db362e78d4b6
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-08 18:47:22 +0900

    edit the function so it will catch errors

--- src/modules/storage/setStorage.js
+++ src/modules/storage/setStorage.js
@@ -1,5 +1,8 @@
 //save objects in the storage as strings
-
-export default function SetStorageItems(key,value){
-    localStorage.setItem(key,JSON.stringify(value))
- };
+export default function setStorageItem(key, value) {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Failed to store item in localStorage:", error);
+    }
+  }
