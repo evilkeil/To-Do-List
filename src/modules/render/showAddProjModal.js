bbbb4473c2fe8934ee185e4840f2eba8ab8f870e
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-08 20:58:28 +0900

    import the close modal function and add it into addproject modal

--- src/modules/render/showAddProjModal.js
+++ src/modules/render/showAddProjModal.js
@@ -1,5 +1,6 @@
 //event listner to the add project btn
 
+import closeModal from '../render/closeModal.js'
 const openbtn = document.getElementById('add-Project');
 const modal = document.getElementById('modal');
 
@@ -9,4 +10,5 @@ export default function ShowProjectAddModal(){
     openbtn.addEventListener('click',function(e){
         modal.showModal();
     })
+    closeModal()
 }
