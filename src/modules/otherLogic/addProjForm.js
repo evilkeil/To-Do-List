98b8b1245ad50c324c742af6a754931ed8806fea
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-08 18:49:58 +0900

    break down the big function in projforms into two aka seperation of concerns

--- src/modules/otherLogic/addProjForm.js
+++ src/modules/otherLogic/addProjForm.js
@@ -1,29 +1,33 @@
 //to handle submit event listener of the modal
+import createProject from './projectObj';
+import checkStorage from '../storage/checkStorage.js';
+import setStorageItem from '../storage/setStorage.js';
 
-import createProject from './projectObj'
 const projectTitleInput = document.getElementById('title');
 const markerInput = document.getElementById('color');
 const form = document.getElementById('add-new-Project');
 const modal = document.getElementById('modal');
 
+function createAndStoreProject() {
+  const title = projectTitleInput.value;
+  const marker = markerInput.value;
+  const currentDate = new Date();
+  const date = currentDate.toISOString().split('T')[0];
+  form.reset();
+  modal.close();
 
+  const projectInfo = createProject(title, marker, date);
+  const storageLength = checkStorage();
+  const projectNumber = storageLength + 1;
 
+  setStorageItem(`project${projectNumber}`, projectInfo);
+
+  return projectInfo;
+}
 
 export default function getNewProjectInfo() {
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        const title = projectTitleInput.value;
-        const marker = markerInput.value;
-        const currentDate = new Date();
-        const date = currentDate.toISOString().split('T')[0];
-        form.reset();
-        modal.close();
-
-       
-        const projectInfo = createProject(title,marker,date);
-
-
-        return projectInfo;
-    });
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    createAndStoreProject();
+  });
 }
