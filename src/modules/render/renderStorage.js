f96f8b353c11064b8e1d7d0a6b5d383e66e1b853
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-09 13:08:05 +0900

    make the renderstorage func only have one condition

--- src/modules/render/renderStorage.js
+++ src/modules/render/renderStorage.js
@@ -4,8 +4,5 @@ import renderEmptymessage from "./renderEmptyMessage.js";
 
 export default  function renderStorage(item){
     if(item === 0){
-      renderEmptymessage();
-    }else{
-        // make it render the projects
-    }
+      renderEmptymessage();}
 }
