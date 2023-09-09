f1c0f4cd3d94f90727b49b8885be07702cdc1661
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-09 13:28:33 +0900

    make a function to render the projects in an arr

---
+++ src/modules/render/renderProjects.js
@@ -0,0 +1,28 @@
+export default function renderProjects(arr){
+    const mainContainer = document.getElementById('main-container');
+    arr.forEach(element => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        //marker
+        const marker = document.createElement('div');
+        marker.classList.add('card-marker');
+        marker.style.cssText = `background-color: ${element.marker};`
+        card.appendChild(marker);
+
+        //title
+        const title = document.createElement('h2');
+        title.textContent=`${element.title}`;
+        card.appendChild(title);
+
+        //time
+        const time = document.createElement('p');
+        time.classList.add('time');
+        time.textContent = `${element.time}`;
+
+
+        mainContainer.appendChild(card)
+    });
+
+    
+}
