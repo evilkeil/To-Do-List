0ad28f4209fc19bb17da0e8750bf6786a1adf387
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-09 13:50:04 +0900

    finish up the pageonload function to render projects or empty message depending on the local storage

--- src/modules/render/pageOnload.js
+++ src/modules/render/pageOnload.js
@@ -1,10 +1,20 @@
 //to check whther theres any projects created by the user in a prev session
+import renderEmptymessage from "./renderEmptyMessage.js";
+import countItemsWithKeyName from "../storage/countItems";
+import renderStorage from "./renderStorage";
+import renderProjects from "./renderProjects.js";
 
-import checkStorage from "../storage/checkStorage";
 
-export default function OnPageLoad(){
+
+export default function OnPageLoad(projectList){
     document.addEventListener('DOMContentLoaded',function(){
-        const items = checkStorage();
+       const items = countItemsWithKeyName(project)
+        if(items === 0){
+            renderEmptymessage();
+        }else{
+            renderProjects(projectList)
+        }
+        
         
     })
 }
