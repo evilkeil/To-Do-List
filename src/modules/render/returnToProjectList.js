2c41aefd3f1a9c0edaebf54aa5ede35f9cb9fbba
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-15 12:59:19 +0900

    write a fucntion to make it retrun from tasks to the project list

--- src/modules/render/returnToProjectList.js
+++ src/modules/render/returnToProjectList.js
@@ -5,7 +5,8 @@ import { CardList, mainContainer } from "../cache/domElements";
 export default function returnToProjectList(target){
    if (target.classList.contains('return') ){
       const taskContainer = document.querySelector('.task-container');
-      console.log(taskContainer)
+      mainContainer.removeChild(taskContainer);
+      mainContainer.appendChild(CardList);
       //   mainContainer.removeChild(taskContainer);
       //   mainContainer.appendChild(CardList);
    }
