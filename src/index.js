0d36ef234d44664d34b790e71328df1e2d12ff94
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-23 16:44:06 +0900

    fix the navigation bar not getting transalted back to the original the reason was i had selected it as a adjecent siblin using the + mark, i shd use general sibling ~ mark to select because sidebar is not the adjecent sibling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './assets/style.css';
+
+const checkbox = document.getElementById('toggle');
+
+const input = checkbox.value;
+
+checkbox.addEventListener('change',function(e){
+    if (checkbox.checked) {
+        // Checkbox is checked
+        console.log('Checkbox is checked');
+        // You can perform additional actions here
+    } else {
+        // Checkbox is unchecked
+        console.log('Checkbox is unchecked');
+        // You can perform additional actions here
+    }
+})
