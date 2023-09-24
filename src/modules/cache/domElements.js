956721232920cebc6087672deb7ede51f9421b71
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-24 21:21:20 +0900

    add data attributes to the task related elements and grab it

--- src/modules/cache/domElements.js
+++ src/modules/cache/domElements.js
@@ -1,6 +1,11 @@
 
-//project list realted elements 
+//project list related elements 
 export const projectContainer = document.querySelector('[data-projectList]');  //the project container
 export const projectForm = document.querySelector('[data-addProjectForm]'); //the add project form
 export const projectInput =document.querySelector('[data-project-input]'); //add project form input
 export const projectDeleteBtn = document.querySelector('[ data-delete-project]');
+
+//task related elements
+export const taskTitle = document.querySelector('[data-task-title]');
+export const taskRemaining = document.querySelector('[data-task-remaining]');
+export const taskList = document.querySelector('[data-task-list]');
