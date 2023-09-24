dbc2f6be6b2879727ad39ec5c565d8ce9f267352
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-24 17:05:46 +0900

    add functionality to the add project form make a factory function to make a new project with name id and task id will be the current date and time made to string make the new project push into the array and render

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './assets/style.css';
 
 
 const projectContainer = document.querySelector('[data-projectList]');
+const projectForm = document.querySelector('[data-addProjectForm]');
+const projectInput =document.querySelector('[data-project-input]');
 
-let projectList = ["studes","sports","Javascript"];
+let projectList = [];
 
 {/* <li class="project">Sports</li> */}
 
@@ -12,7 +14,7 @@ function renderProjectList(){
     projectList.forEach(project =>{
         const li = document.createElement('li');
         li.classList.add('project');
-        li.innerText=project;
+        li.innerText=project.name;
         projectContainer.appendChild(li);
         
     })
@@ -25,4 +27,23 @@ function clearList(element){
     }
 }
 
+projectForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(projectInput.value === null ||projectInput.value === "" ) return;
+    const newProject = createProject(projectInput.value);
+    projectList.push(newProject);
+    renderProjectList()
+    projectForm.reset()
+
+    
+
+})
+
+function createProject(name){
+    return {
+        name:name,
+        id:Date.now().toString(),
+        tasks:[],
+    }
+}
 renderProjectList();
