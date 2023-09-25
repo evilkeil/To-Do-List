3cf56993fb1c385e06d443e95892fe54be9f55c8
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-25 13:44:32 +0900

    add functionality to task input

--- src/modules/eventListeners/addTasks.js
+++ src/modules/eventListeners/addTasks.js
@@ -1,3 +1,29 @@
+import { taskForm, taskInput } from "../cache/domElements";
+import { PROJECT_KEY, PROJECT_SELECTED_KEY } from "../cache/storageKeys";
+import createTask from "../factoryFunctions/createTask";
+import render from "../render/render";
+import GetStorageItems from "../storage/getStorage";
+import setStorageItem from "../storage/setStorage";
+
 export default function addTask(){
     
+    taskForm.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const arr = GetStorageItems(PROJECT_KEY);
+        const selected =GetStorageItems(PROJECT_SELECTED_KEY);
+        const value = taskInput.value;
+    if (value === null || value === undefined) return;
+        const task = createTask(value);
+        arr.forEach(element => {
+            if (element.id === selected){
+               element.tasks.push(task);
+               
+                
+            }
+        });
+        console.log(arr);
+       setStorageItem(PROJECT_KEY,arr);
+       render(arr,selected);
+    
+    })
 }
--- src/modules/eventListeners/attachEventListners.js
+++ src/modules/eventListeners/attachEventListners.js
@@ -1,9 +1,11 @@
 import addProject from "./addproject";
+import addTask from "./addTasks";
 import deleteProject from "./deleteProject";
 import selectProject from "./selectProject";
 
 export default function attachEventListners(arr,selected){
     addProject(arr,selected);
     selectProject(arr,selected);
+    addTask();
     deleteProject();
 }
