548a4992b676e6a8ffcc1a3bdf15dab3b8fde264
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-25 13:18:12 +0900

    make a new factory function for tasks

--- src/modules/factoryFunctions/createProject.js
+++ src/modules/factoryFunctions/createProject.js
@@ -2,11 +2,12 @@ export default function createProject(name){
     return {
         name:name,
         id:Date.now().toString(),
-        tasks:[{
-            name:"test",
-            id:1,
-            complete:true
-        },
+        tasks:[
+        //     {
+        //     name:"test",
+        //     id:1,
+        //     complete:true
+        // },
        
     ],
     }
