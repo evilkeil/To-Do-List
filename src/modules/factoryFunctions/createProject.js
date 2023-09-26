38dddda7ddaceb2fe61518b8e25277e8e7926bbd
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-09-26 20:51:53 +0900

    remove commented out code

--- src/modules/factoryFunctions/createProject.js
+++ src/modules/factoryFunctions/createProject.js
@@ -3,12 +3,6 @@ export default function createProject(name){
         name:name,
         id:Date.now().toString(),
         tasks:[
-        //     {
-        //     name:"test",
-        //     id:1,
-        //     complete:true
-        // },
-       
     ],
     }
 }
